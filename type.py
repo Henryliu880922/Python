05ff3106735327cfbade8891b64f90e54f8d47f1
Henryliu880922 <es13463077@gmail.com>
2022-04-25 21:39:52 +0800

    Update type.py

--- type.py
+++ type.py
@@ -6,8 +6,28 @@ print(0x41) #65
 
 print(3.14+3.14)
 
+#complex 複數
+print(2+1J)
+
 #boolean
 
 print(1<2)# 列出 真
 
 print(1>2)# 列出 假
+
+#str
+
+print('''
+星期一
+星期二
+星期三''')
+
+#list
+
+print ([1,"Taipei" ,2,"Tokyo"])
+
+#資料型別
+print(type(1000))
+print(type(2.2))
+print(type(2+1J))
+print(type(False))
