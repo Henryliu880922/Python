7b4a6ff1ecfefaa23c48c1ee871ecdc8a92400a1
Henryliu880922 <es13463077@gmail.com>
2022-04-27 20:31:39 +0800

    Create function.py

---
+++ 第三章/function.py
@@ -0,0 +1,28 @@
+#內建函式 abs 絕對值
+print(abs(5))
+print(abs(-1.2))
+
+# min 最小值
+print(min(5,1))
+print(min(-5,-1,3,8,9))
+
+# max 最大值
+print(max(1,10,5,2))
+
+# hex 十進位轉十六進位
+print(hex(255))
+print(hex(65))
+
+#oct 十進位轉八進位
+print(oct(65))
+
+# bin 十進位轉二進位
+print(bin(65))
+print(bin(10))
+
+#int 整數部分
+print(int(3.6))
+
+# round 數值最近的整數
+print(round(3.6))
+print
