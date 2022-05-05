6c07ee744092ec87f24df5a615d8ebece9d850bc
Henryliu880922 <es13463077@gmail.com>
2022-05-05 17:29:07 +0800

    Create 字串轉換方法.py

---
+++ 第三章/字串轉換方法.py
@@ -0,0 +1,7 @@
+x="Hello World"
+print(str.upper(x)) # 所有字元轉換成大寫，亦可寫成x.upper()
+print(str.lower(x)) # 所有字元轉換成小寫，亦可寫成x.lower()
+print(str.swapcase(x)) # 大小寫互換
+print(str.replace("World","Mary")) # 將World取代成Mary
+print(str.capitalize("an egg")) # 第一個字元轉換成大寫，亦可寫成"an egg".capitalize()
+print(str.title("an egg")) # 每個單字第一個字元轉換成大寫
