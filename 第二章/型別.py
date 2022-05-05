1eb8f7249c68fd31b16a60b8f72dabe5629b6c64
Henryliu880922 <es13463077@gmail.com>
2022-05-05 14:16:16 +0800

    Create 型別.py

---
+++ 第二章/型別.py
@@ -0,0 +1,45 @@
+# int
+
+print(0x41) #65
+
+#float
+
+print(3.14+3.14)
+
+#complex 複數
+print(2+1J)
+
+#boolean
+
+print(1<2)# 列出 真
+
+print(1>2)# 列出 假
+
+#str
+
+print('''
+星期一
+星期二
+星期三''')
+
+#list 串列
+
+print ([1,"Taipei" ,2,"Tokyo"])
+
+# tuple 序對
+
+print((1,"Tapei",2,"Tokyo"))
+
+#set 集合
+
+print({1,"台北",2,"東京"})
+
+#dict 字典
+
+print({"ID":"1","name":"小丸子"})
+
+#資料型別
+print(type(1000))
+print(type(2.2))
+print(type(2+1J))
+print(type(False))
