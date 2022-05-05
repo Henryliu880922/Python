43c86721751e2a7d90d2ee04eab60a2c9f208e2a
Henryliu880922 <es13463077@gmail.com>
2022-05-05 21:43:48 +0800

    Create EnglishNum.py

---
+++ 第四章/範例/EnglishNum.py
@@ -0,0 +1,13 @@
+num=eval(input("請輸入1~5的整數："))
+if num==1:
+    print("One")
+elif num==2:
+    print("Two")
+elif num==3:
+    print("Three")
+elif num==4:
+    print("Four")
+elif num==5:
+    print("Five")
+else:
+    print("Not in Range")
