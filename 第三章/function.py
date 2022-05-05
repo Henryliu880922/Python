e275285b16cb10124a4ecc6ef22d0349f3f75af6
Henryliu880922 <es13463077@gmail.com>
2022-05-05 11:02:05 +0800

    Update function.py

--- 第三章/function.py
+++ 第三章/function.py
@@ -13,16 +13,26 @@ print(max(1,10,5,2))
 print(hex(255))
 print(hex(65))
 
-#oct 十進位轉八進位
+# oct 十進位轉八進位
 print(oct(65))
 
 # bin 十進位轉二進位
 print(bin(65))
 print(bin(10))
 
-#int 整數部分
+# int 整數部分
 print(int(3.6))
 
 # round 數值最近的整數
 print(round(3.6))
-print
+print(round(-3.6))
+print(round(2.678,2))
+
+# pow 傳回數值參數X的數值參數Y次方值
+print(pow(2,10))
+
+# float 傳回字串參數X轉換成浮點數的結果
+print(float("1.23"))
+
+# complex 傳回字串參數X轉換成複數的結果
+print(complex("1+2j"))   #這個函數的字串參數裡面不能包含空白
