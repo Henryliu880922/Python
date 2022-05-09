9ec2d4fbb0c7410bcee89f7ce567d2b2ea21994e
Henryliu880922 <es13463077@gmail.com>
2022-05-09 22:24:28 +0800

    Create 字串處理方法.py

---
+++ 編譯好的檔案/第三章/字串處理方法/字串處理方法.py
@@ -0,0 +1,21 @@
+
+x=100 # 令變數X指向一個值為100的int物件
+id(x) # 取得變數X指向之物件的id編號
+print(id(x)) # 印出這個id編號
+
+type(x) # 取得變數X指向之物件的型別
+print(type(x)) # 印出這個型別
+
+print(x) # 取得變數X指向之物件的值
+# print(print(x)) 印出這個值 且等同於print(x)
+
+
+y="Happy" # 令變數Y指向一個值為Happy的str物件
+id(y) # 取得變數Y指向之物件的id編號
+print(id(y)) # 印出這個id編號
+
+type(y) # 取得變數Y指向之物件的型別
+print(type(y)) # 印出這個型別
+
+print(y) # 取得變數Y指向之物件的值
+# print(print(y)) 印出這個值 且等同於print(y)
