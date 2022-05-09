cfe56bfc23af5257cdd570b1872475285c945155
Henryliu880922 <es13463077@gmail.com>
2022-05-09 22:24:39 +0800

    Create 搜尋子字串方法.py

---
+++ 編譯好的檔案/第三章/搜尋子字串方法/搜尋子字串方法.py
@@ -0,0 +1,8 @@
+x="WowWowWowWowWow"
+print(x.count("Wow")) # 字串中出現Wow的次數
+print(x.startswith("Wow")) # 字串是否以Wow開頭
+print(x.startswith("Ha"))
+print(x.endswith("Wow")) # 字串是否以Wow結尾
+print(x.endswith("Ha"))
+print(x.find("Wow")) # Wow出現在字串中的最小索引
+print(x.rfind("Wow")) # Wow出現在字串中的最大索引
