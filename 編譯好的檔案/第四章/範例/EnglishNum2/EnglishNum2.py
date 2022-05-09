6b990dce71c54edd2a30857be8c5e198a3227bd4
Henryliu880922 <es13463077@gmail.com>
2022-05-09 22:29:02 +0800

    Create EnglishNum2.py

---
+++ 編譯好的檔案/第四章/範例/EnglishNum2/EnglishNum2.py
@@ -0,0 +1,17 @@
+num=eval(input("請輸入1~5的整數："))
+if num==1:
+    print("One")
+else:
+    if num==2:
+        print("Two")
+    else:
+        if num==3:
+            print("Three")
+        else:
+                if num==4:
+                    print("Four")
+                else:
+                    if num==5:
+                        print("Five")
+                    else:
+                        print("Not in Range")
