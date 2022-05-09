3941c9fa014e2bd1ab26d9d805a4fa6bc9de4ffa
Henryliu880922 <es13463077@gmail.com>
2022-05-09 22:24:02 +0800

    Create 內建數值函式.py

---
+++ 編譯好的檔案/第三章/內建數值函式/內建數值函式.py
@@ -0,0 +1,38 @@
+#內建函式 abs 絕對值
+print(abs(5))
+print(abs(-1.2))
+
+# min 最小值
+print(min(5,1))
+print(min(-5,-1,3,8,9))
+
+# max 最大值
+print(max(1,10,5,2))
+
+# hex 十進位轉十六進位
+print(hex(255))
+print(hex(65))
+
+# oct 十進位轉八進位
+print(oct(65))
+
+# bin 十進位轉二進位
+print(bin(65))
+print(bin(10))
+
+# int 整數部分
+print(int(3.6))
+
+# round 數值最近的整數
+print(round(3.6))
+print(round(-3.6))
+print(round(2.678,2))
+
+# pow 傳回數值參數X的數值參數Y次方值
+print(pow(2,10))
+
+# float 傳回字串參數X轉換成浮點數的結果
+print(float("1.23"))
+
+# complex 傳回字串參數X轉換成複數的結果
+print(complex("1+2j"))   #這個函數的字串參數裡面不能包含空白
